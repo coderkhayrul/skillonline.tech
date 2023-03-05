b16fda8e23d6ab0da5af8811f8affcc45a77e976
coderkhayrul <coderkhayrul@gmail.com>
2023-03-05 12:28:57 +0600

    Sidebar Problem Fixing

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -5,7 +5,6 @@
 @section('backend-content')
     <div class="page-content">
         <div class="container-fluid">
-
             <!-- start page title -->
             <div class="row">
                 <div class="col-12">
--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -71,40 +71,18 @@
                     <ul class="sub-menu" aria-expanded="false">
                         <li>
                             <a href="{{ route('admin.setting.analytic.index') }}" class="waves-effect">
-                                 <span key="t-analytic">Analytic</span>
+                                <span key="t-analytic">Analytic</span>
                             </a>
                         </li>
                     </ul>
                     <ul class="sub-menu" aria-expanded="false">
                         <li>
                             <a href="{{ route('admin.setting.basicsetting.index') }}" class="waves-effect">
-                                 <span key="t-basic">Basic</span>
+                                <span key="t-basic">Basic</span>
                             </a>
                         </li>
                     </ul>
                 </li>
-
-
-
-
-
-
-
-                {{-- <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="bx bx-home-circle"></i>
-                        <span key="t-dashboards">Dashboards</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="index.html" key="t-default">Default</a></li>
-                        <li><a href="dashboard-saas.html" key="t-saas">Saas</a></li>
-                        <li><a href="dashboard-crypto.html" key="t-crypto">Crypto</a></li>
-                        <li><a href="dashboard-blog.html" key="t-blog">Blog</a></li>
-                        <li><a href="dashboard-job.html"><span class="badge rounded-pill text-bg-success float-end"
-                                    key="t-new">New</span> <span key="t-jobs">Jobs</span></a></li>
-                    </ul>
-                </li> --}}
-
             </ul>
         </div>
         <!-- Sidebar -->
